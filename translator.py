528dcdda275f55c265b647870a5f15aa31562d05
ABINASH BISWAL KONERSMAN <abinash.konersman@state.co.us>
2020-12-21 18:16:12 -0700

    Added Road_Event_Feed_Info according to WZDX_object_Diagram of USDOT

--- translator.py
+++ translator.py
@@ -1,3 +1,4 @@
+
 import xmltodict
 import xml.etree.ElementTree as ET
 import json
@@ -5,3 +6,19 @@ from datetime import datetime
 import uuid
 import random
 import string
+
+
+# Translator
+def wzdx_creator(messages, info):
+    wzd = {}
+    wzd['road_event_feed_info'] = {}
+    # hardcode
+    wzd['road_event_feed_info']['feed_info_id'] = info['feed_info_id']
+    wzd['road_event_feed_info']['update_date'] = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+    wzd['road_event_feed_info']['publisher'] = 'CDOT '
+    wzd['road_event_feed_info']['contact_name'] = 'Abinash Konersman'
+    wzd['road_event_feed_info']['contact_email'] = 'abinash.konersman@state.co.us'
+    if info['metadata'].get('datafeed_frequency_update', False):
+        wzd['road_event_feed_info']['update_frequency'] = info['metadata'][
+            'datafeed_frequency_update']  # Verify data type
+    wzd['road_event_feed_info']['version'] = '3.0'
