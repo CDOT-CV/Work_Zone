89967fc3d3cfd78d152afe8e078759c269f0e002
ABINASH BISWAL KONERSMAN <abinash.konersman@state.co.us>
2020-12-22 10:27:46 -0700

    commit

--- translator/source code/translator.py
+++ translator/source code/translator.py
@@ -22,3 +22,18 @@ def wzdx_creator(messages, info):
         wzd['road_event_feed_info']['update_frequency'] = info['metadata'][
             'datafeed_frequency_update']  # Verify data type
     wzd['road_event_feed_info']['version'] = '3.0'
+
+
+    data_source = {}
+    data_source['data_source_id'] = str(uuid.uuid4())
+    data_source['feed_info_id'] = info['feed_info_id']
+    data_source['organization_name'] = info['metadata']['issuing_organization']
+    data_source['contact_name'] = info['metadata']['contact_name']
+    data_source['contact_email'] = info['metadata']['contact_email']
+    if info['metadata'].get('datafeed_frequency_update', False):
+        data_source['update_frequency'] = info['metadata']['datafeed_frequency_update']
+    data_source['update_date'] = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+    data_source['location_verify_method'] = info['metadata']['location_verify_method']
+    data_source['location_method'] = info['metadata']['wz_location_method']
+    data_source['lrs_type'] = info['metadata']['lrs_type']
+    # data_source['lrs_url'] = "basic url"
