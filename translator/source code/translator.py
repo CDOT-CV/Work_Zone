742ce74d6dfbf71d0bb27a80363cdf409ee1eec2
ABINASH BISWAL KONERSMAN <abinash.konersman@state.co.us>
2020-12-29 15:39:45 -0700

    test

--- translator/source code/translator.py
+++ translator/source code/translator.py
@@ -321,32 +321,33 @@ def parse_arguments(argv):
 
 inputfile,outputfile=parse_arguments(sys.argv[1:])
 
+if  inputfile :
 # Added encoding argument because of weird character at start of incidents.xml file
-with open(inputfile, encoding='utf-8-sig') as frsm:
-    # Read
-    xmlSTRING = frsm.read()
-    icone_obj = xmltodict.parse(xmlSTRING)
+    with open(inputfile, encoding='utf-8-sig') as frsm:
+        # Read
+        xmlSTRING = frsm.read()
+        icone_obj = xmltodict.parse(xmlSTRING)
 
-    info = {}
+        info = {}
 
-    #### Consider whether this id needs to be hardcoded or generated
-    info['feed_info_id'] = "feed_info_id"
+        #### Consider whether this id needs to be hardcoded or generated
+        info['feed_info_id'] = "feed_info_id"
 
-    #### This information is required, might want to hardcode
-    info['metadata'] = {}
-    info['metadata']['wz_location_method'] = "wz_location_method"
-    info['metadata']['lrs_type'] = "lrs_type"
-    info['metadata']['location_verify_method'] = "location_verify_method"
-    info['metadata']['datafeed_frequency_update'] = 86400
-    info['metadata']['timestamp_metadata_update'] = "timestamp_metadata_update"
-    info['metadata']['contact_name'] = "contact_name"
-    info['metadata']['contact_email'] = "contact_email"
-    info['metadata']['issuing_organization'] = "issuing_organization"
+        #### This information is required, might want to hardcode
+        info['metadata'] = {}
+        info['metadata']['wz_location_method'] = "wz_location_method"
+        info['metadata']['lrs_type'] = "lrs_type"
+        info['metadata']['location_verify_method'] = "location_verify_method"
+        info['metadata']['datafeed_frequency_update'] = 86400
+        info['metadata']['timestamp_metadata_update'] = "timestamp_metadata_update"
+        info['metadata']['contact_name'] = "contact_name"
+        info['metadata']['contact_email'] = "contact_email"
+        info['metadata']['issuing_organization'] = "issuing_organization"
 
 
-    wzdx = wzdx_creator(icone_obj, info)
-    with open(outputfile, 'w') as fwzdx:
-        fwzdx.write(json.dumps(wzdx, indent=2))
+        wzdx = wzdx_creator(icone_obj, info)
+        with open(outputfile, 'w') as fwzdx:
+            fwzdx.write(json.dumps(wzdx, indent=2))
 
 
 #Unit testing code
