ec32f7334a5193615cd553743309641232278105
ABINASH BISWAL KONERSMAN <dougabi@yahoo.com>
2021-03-17 14:30:17 -0500

    renamed function

--- test/test_main.py
+++ test/test_main.py
@@ -128,7 +128,7 @@ def test_get_ftp_credentials_no_env_vars(secret):
     'icone_ftp_password_secret_name': 'fail',
     'project_id': 'project_id'})
 @patch('google.cloud.secretmanager.SecretManagerServiceClient')
-def test_get_ftp_secrets_username_password_does_not_exist(secret):
+def test_get_ftp_secrets_password_does_not_exist(secret):
     secret().access_secret_version = fake_secret_client
     actual = main.get_ftp_credentials()
     
@@ -139,7 +139,7 @@ def test_get_ftp_secrets_username_password_does_not_exist(secret):
     'icone_ftp_password_secret_name': 'secret_password',
     'project_id': 'project_id'})
 @patch('google.cloud.secretmanager.SecretManagerServiceClient')
-def test_get_ftp_secrets_does_not_exist(secret):
+def test_get_ftp_secrets_username_does_not_exist(secret):
     secret().access_secret_version = fake_secret_client
     actual = main.get_ftp_credentials()
     
