b5e287ffc3328c04f07530ec452e11783c802c89
jacob6838 <51723791+jacob6838@users.noreply.github.com>
2021-11-24 19:56:45 -0700

    updating build number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import pathlib
 from setuptools import setup, find_packages
 
-VERSION = {{__BuildNumber__}}
+VERSION = "0.2.2"
 DESCRIPTION = 'CDOT Work Zone WZDx Translators'
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
