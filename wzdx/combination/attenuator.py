82b6af70a5aa401089134f39a96a1af9ca0cf11f
jacob6838 <51723791+jacob6838@users.noreply.github.com>
2022-05-04 13:24:31 -0600

    Create attenuator.py

---
+++ wzdx/combination/attenuator.py
@@ -0,0 +1,10 @@
+def main():
+    raise NotImplementedError("main method not implemented")
+
+
+def combine_with_planned_event(attenuator_obj, planned_event_wzdx_feature):
+    raise NotImplementedError("main method not implemented")
+
+
+if __name__ == "__main__":
+    main()
