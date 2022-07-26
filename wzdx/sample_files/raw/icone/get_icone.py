d6f5ae4b2af72b3459f91fc4b5883c712c258de9
jacob6838 <51723791+jacob6838@users.noreply.github.com>
2022-07-26 13:27:13 -0600

    adding directories

--- wzdx/sample_files/raw/icone/get_icone.py
+++ wzdx/sample_files/raw/icone/get_icone.py
@@ -1,5 +1,8 @@
 import urllib.request
 import datetime
+import os
+import wzdx
+import json
 
 ICONE_USERNAME = 'cdot'
 ICONE_FILE_PATH = 'incidents-extended.xml'
@@ -21,7 +24,26 @@ def get_ftp_file_contents():
     return file_contents
 
 
-with open(f'icone_ftp_{datetime.datetime.now().strftime("%Y%m%d-%H%M%S")}.xml', 'w+', newline='') as f:
-    icone = get_ftp_file_contents()
+now = datetime.datetime.now()
+date_directory = now.strftime("%Y_%m_%d")
+directories = [date_directory, f'{date_directory}/raw',
+               f'{date_directory}/standard', f'{date_directory}/enhanced']
+for dir in directories:
+    if not os.path.exists(date_directory):
+        os.makedirs(date_directory)
+
+icone = get_ftp_file_contents()
+
+with open(f'{date_directory}/raw/raw_{now.strftime("%Y%m%d-%H%M%S")}.xml', 'w', newline='') as f:
     f.write(icone)
-    print(icone)
+
+standard_msgs = wzdx.raw_to_standard.icone.generate_standard_messages_from_string(
+    icone)
+
+with open(f'{date_directory}/standard/standard_{now.strftime("%Y%m%d-%H%M%S")}.xml', 'w', newline='') as f:
+    f.write(json.dumps(standard_msgs), indent=2)
+
+wzdx = wzdx.standard_to_enhanced.icone_translator.wzdx_creator(standard)
+
+with open(f'{date_directory}/wzdx/wzdx_{now.strftime("%Y%m%d-%H%M%S")}.xml', 'w', newline='') as f:
+    f.write(json.dumps(standard), indent=2)
